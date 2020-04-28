6b032e447ce44323c3e038516a814bde000be7cf
Friz64 <friz64mc@gmail.com>
2020-04-28 20:41:00 +0200

    Check for Vulkan version in `pointer-chain`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Check for supported Vulkan version in `pointer-chain` example
 
 ## [0.5.1+139] - 2020-04-28
 ### Changed
--- erupt-examples/src/bin/pointer_chain.rs
+++ erupt-examples/src/bin/pointer_chain.rs
@@ -1,4 +1,5 @@
 use erupt::{vk1_0::*, vk1_1::*, vk1_2::*, CoreLoader, InstanceLoader};
+use std::ffi::CStr;
 
 fn main() {
     let mut core = CoreLoader::new().unwrap();
@@ -14,7 +15,7 @@ fn main() {
 
     let info = ApplicationInfo::default()
         .builder()
-        .api_version(erupt::make_version(1, 2, 0));
+        .api_version(erupt::make_version(1, 1, 0));
     let instance_info = InstanceCreateInfo::default()
         .builder()
         .application_info(&info);
@@ -26,26 +27,45 @@ fn main() {
     .unwrap();
     instance.load_vk1_0().unwrap();
     instance.load_vk1_1().unwrap();
-    println!("Instance: {:p}", instance.handle);
 
     let physical_devices = unsafe { instance.enumerate_physical_devices(None) }.unwrap();
-    let physical_device = physical_devices[0];
-    println!("Physical Device: {:p}", physical_device);
 
-    let mut features2 = PhysicalDeviceFeatures2::default();
-    let mut vk11features = PhysicalDeviceVulkan11Features::default();
-    let mut vk12features = PhysicalDeviceVulkan12Features::default();
-    unsafe {
-        vk11features.extend(&mut features2);
-        vk12features.extend(&mut features2);
+    for physical_device in physical_devices {
+        let properties = unsafe { instance.get_physical_device_properties(physical_device, None) };
+        println!("Physical device: {:?}", unsafe {
+            CStr::from_ptr(properties.device_name.as_ptr())
+        });
+
+        let vk11 = properties.api_version >= erupt::make_version(1, 1, 0);
+        let vk12 = properties.api_version >= erupt::make_version(1, 2, 0);
+
+        if vk11 {
+            let mut features2 = PhysicalDeviceFeatures2::default();
+            let mut vk11features = PhysicalDeviceVulkan11Features::default();
+            let mut vk12features = PhysicalDeviceVulkan12Features::default();
+            unsafe {
+                vk11features.extend(&mut features2);
+
+                if vk12 {
+                    vk12features.extend(&mut features2);
+                }
+            }
+
+            let features2 =
+                unsafe { instance.get_physical_device_features2(physical_device, Some(features2)) };
+
+            println!("Vulkan 1.0 Features: {:?}", features2.features);
+            println!("Vulkan 1.1 Features: {:?}", vk11features);
+
+            if vk12 {
+                println!("Vulkan 1.2 Features: {:?}", vk12features);
+            }
+        } else {
+            println!("Vulkan 1.1 required");
+        }
+
+        println!();
     }
-
-    let features2 =
-        unsafe { instance.get_physical_device_features2(physical_device, Some(features2)) };
-
-    println!("Vulkan 1.0 Features: {:?}", features2.features);
-    println!("Vulkan 1.1 Features: {:?}", vk11features);
-    println!("Vulkan 1.2 Features: {:?}", vk12features);
 
     unsafe {
         instance.destroy_instance(None);
